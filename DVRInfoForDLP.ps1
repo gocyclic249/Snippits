0e11eaf98c404e997ebfc3b4238e5de50549dcfe
Daniel <gocyclic249@users.noreply.github.com>
2026-05-05 06:46:14 -0600

    Create DVRInfoForDLP.ps1 to gather DLP waiver info
    This script gathers information required for the DLP waiver, including device details and user properties.

---
+++ DVRInfoForDLP.ps1
@@ -0,0 +1,15 @@
+# Gets the info required for the DLP waiver at lease in the AF and USSF
+
+Get-PnpDevice -class CDROM -PresentOnly  -ErrorAction SilentlyContinue| Select-Object InstanceID,"Blank1","Blank2","Blank3",FriendlyName | ConvertTo-Csv -Delimiter "`t" -NoTypeInformation | Select-Object -Skip 1
+Get-PnpDevice -class DiskDrive -PresentOnly -ErrorAction SilentlyContinue| Select-Object InstanceID,"Blank1","Blank2","Blank3",FriendlyName | ConvertTo-Csv -Delimiter "`t" -NoTypeInformation | Select-Object -Skip 1
+
+$searcher = New-Object System.DirectoryServices.DirectorySearcher
+$searcher.Filter = "(samaccountname=$env:USERNAME)"
+$user = $searcher.FindOne()
+$user.Properties["givenname"]
+$user.Properties["sn"]
+$user.Properties["mail"]
+$user.Properties["employeeid"]
+Get-WmiObject -Class Win32_ComputerSystem | Select-Object Manufacturer, Model
+Get-WmiObject -Class Win32_CDROMDrive | Select-Object Manufacturer, Model
+Get-WmiObject -Class Win32_DiskDrive | Select-Object Manufacturer, Model
